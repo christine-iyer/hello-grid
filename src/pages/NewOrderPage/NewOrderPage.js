629648df24cfef13e318ae10747f2a6725067ee1
chrisIyer <hellohotbox420@gmail.com>
2023-06-30 15:22:24 -0400

    breaking into new categories.

--- src/pages/NewOrderPage/NewOrderPage.js
+++ src/pages/NewOrderPage/NewOrderPage.js
@@ -4,24 +4,63 @@ import * as ordersAPI from '../../utilities/order-api';
 import styles from './NewOrderPage.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import MenuList from '../../components/MenuList/MenuList';
-import CategoryList from '../../components/CategoryList/CategoryList';
+import AccountNumList from '../../components/AccountNumList/AccountNumList';
+import DescriptionList from '../../components/DescriptionList/DescriptionList';
+import PayeeList from '../../components/PayeeList/PayeeList';
+import UsesList from '../../components/UsesList/UsesList';
 import OrderDetail from '../../components/OrderDetail/OrderDetail';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
 export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
-  const [activeCat, setActiveCat] = useState('');
+  const [activeAcctNum, setActiveAcctNum] = useState('');
+  const [activeDescription, setActiveDescription] = useState('');
+  const [activePayee, setActivePayee] = useState('');
+  const [activeUses, setActiveUses] = useState('');
+
   const [cart, setCart] = useState(null);
-  const categoriesRef = useRef([]);
+  
+  const accountNumsRef = useRef([]);
+  const descriptionsRef = useRef([]);
+  const payeesRef = useRef([]);
+  const usesRef = useRef([]);
+
   const navigate = useNavigate();
 
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+
+
+
+
+      accountNumsRefRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
+
+
+
       categoriesRef.current = items.reduce((cats, item) => {
         const cat = item.category.name;
         return cats.includes(cat) ? cats : [...cats, cat];
       }, []);
+
+
+
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
+
+
+
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
+
+
       setMenuItems(items);
       setActiveCat(categoriesRef.current[0]);
     }
